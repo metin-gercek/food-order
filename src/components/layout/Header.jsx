3eb7653daf97751d0616cefa53fbddc393f2946b
metin.gercek <metingercek@hotmail.com>
2023-05-19 09:22:04 +0300

    responsive hamburder menu

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,16 +4,22 @@ import OutsideClickHandler from "react-outside-click-handler";
 import Logo from "./ui/Logo";
 import Title from "./ui/Title";
 import Search from "./ui/Search";
+import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false);
+  const [isMenuModal, setIsMenuModal] = useState(false);
 
   return (
     <div className="h-[5.5rem] bg-secondary">
       <div className="container mx-auto text-white flex justify-between items-center h-full">
         <Logo />
-        <nav>
-          <ul className="flex gap-x-2">
+        <nav
+          className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${
+            isMenuModal === true && "!grid place-content-center"
+          }`}
+        >
+          <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
               <a href="">Home</a>
             </li>
@@ -27,6 +33,14 @@ const Header = () => {
               <a href="">Book Table</a>
             </li>
           </ul>
+          {isMenuModal && (
+            <button
+              className="absolute  top-4 right-4 z-50"
+              onClick={() => setIsMenuModal(false)}
+            >
+              <GiCancel size={25} className=" transition-all" />
+            </button>
+          )}
         </nav>
         <div className="flex gap-x-4 items-center">
           <a href="#">
@@ -38,13 +52,16 @@ const Header = () => {
           <button onClick={() => setIsSearchModal(true)}>
             <FaSearch className="hover:text-primary transition-all" />
           </button>
-          <a href="#">
+          <a href="#" className="md:inline-block hidden sm">
             <button className="btn-primary">Order Online</button>
           </a>
+          <button className="sm:hidden inline-block"
+            onClick={() => setIsMenuModal(true)}>
+            <GiHamburgerMenu className="hover:text-primary transition-all" />
+          </button>
         </div>
       </div>
-      {isSearchModal && 
-        <Search setIsSearchModal={setIsSearchModal}/>}
+      {isSearchModal && <Search setIsSearchModal={setIsSearchModal} />}
     </div>
   );
 };
