b6e7951cfa3d4f898d3c6e48598ea167cda43f30
metin.gercek <metingercek@hotmail.com>
2023-05-25 12:48:20 +0300

    add menu links

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Logo = () => {
   return (
-    <div className='text-[2rem] font-dancing font-bold'>Logo</div>
-  )
-}
+    <Link href="/" className="text-[2rem] font-dancing font-bold">
+      Metin
+    </Link>
+  );
+};
 
-export default Logo
+export default Logo;
---
+++ src/pages/about/index.jsx
@@ -0,0 +1,12 @@
+import About from '../../components/About'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <React.Fragment>
+        <About/>
+    </React.Fragment>
+  )
+}
+
+export default Index
