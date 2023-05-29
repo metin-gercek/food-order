f1e99c85fc2d49db6e8e2e82bec15c314802afcd
metin.gercek <metingercek@hotmail.com>
2023-05-29 14:57:58 +0300

    fix product page responsive

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -4,8 +4,8 @@ import Title from "../../components/ui/Title";
 
 const Index = () => {
   return (
-    <div className="flex items-center h-screen gap-20 py-20 flex-wrap ">
-      <div className="relative md:flex-1 w-[80%] h-[80%] mx-20">
+    <div className="flex items-center md:h-screen gap-5 py-20 flex-wrap ">
+      <div className="relative md:flex-1 md:w-[80%] md:h-[80%] w-64 h-64  mx-auto">
         <Image src="/images/pizza3.png" alt="" fill style={{objectFit:"contain"}} />
       </div>
       <div className="md:flex-1 md:text-start text-center">
