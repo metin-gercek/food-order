e2384483fb8527b85f5972b54e08fdf28e4f9b44
metin.gercek <metingercek@hotmail.com>
2023-05-30 15:44:56 +0300

    create redux toolkit and shopping cart slice

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,6 +5,7 @@ import Search from "../ui/Search";
 import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false);
@@ -12,6 +13,7 @@ const Header = () => {
 
   const router = useRouter();
   // console.log(router);
+  const cart = useSelector((state) => state.cart);
 
   return (
     <div
@@ -56,8 +58,11 @@ const Header = () => {
             </span>{" "}
           </Link>
           <Link href="/cart">
-            <span>
+            <span className="relative">
               <FaShoppingCart className="hover:text-primary transition-all cursor-pointer" />
+              <span className="w-4 h-4 text-xs grid place-content-center rounded-full bg-primary absolute -top-2 -right-3 text-black font-bold">
+                {cart.products.length === 0 ? "0" : cart.products.length}
+              </span>
             </span>{" "}
           </Link>
           <button onClick={() => setIsSearchModal(true)}>
--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Image from "next/image";
 import Title from "../../components/ui/Title";
+import { addProduct } from "../../redux/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Index = () => {
   const extraItemList = [
@@ -24,11 +26,30 @@ const Index = () => {
       name: "Onions",
       price: 1,
     },]
+
+    const foodItems = [
+      {
+        id: 1,
+        name: "Pizza 1",
+        price: 8,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit neque, repudiandae aperiam, harum at blanditiis ipsam alias quod esse, animi dolor voluptatum aut recusandae illo?",
+        extraOptions: [
+          {
+            id: 1,
+            name: "Mozzarella",
+            price: 1,
+          },
+        ],
+      },
+    ]
     const [prices, setPrices] = useState([8, 12, 16]);
     const [price, setPrice] = useState(prices[0]);
     const [size, setSize] = useState(0);
     const [extraItems, setExtraItems] = useState(extraItemList);
     const [extras, setExtras] = useState([]);
+    const dispatch = useDispatch();
+
+    const cart = useSelector((state) => state.cart);
   
     const handleSize = (sizeIndex) => {
       const difference = prices[sizeIndex] - prices[size];
@@ -51,7 +72,12 @@ const Index = () => {
         setExtras(extras.filter((extra) => extra.id !== item.id));
       }
     };
-    console.log(extras);
+
+    const handleClick = () => {
+      dispatch(addProduct({...foodItems[0], size, extras, quatity: 1}));
+      
+    }
+    console.log(cart);
 
   return (
     <div className="flex items-center md:h-[calc(100vh_-_88px)] gap-5; py-20 flex-wrap ">
@@ -109,7 +135,7 @@ const Index = () => {
             </label>
           ))}
         </div>
-        <button className="btn-primary">Add to Cart</button>
+        <button className="btn-primary" onClick={handleClick}>Add to Cart</button>
       </div>
     </div>
   );
