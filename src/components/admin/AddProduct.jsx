3612d791ea3bfc0e7ac7c6bfaa8df052082c2198
metin.gercek <metingercek@hotmail.com>
2023-06-09 23:55:48 +0300

    add new product

--- src/components/admin/AddProduct.jsx
+++ src/components/admin/AddProduct.jsx
@@ -5,10 +5,35 @@ import Title from "../ui/Title";
 import { GiCancel } from "react-icons/gi";
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const AddProduct = ({ setIsProductModal }) => {
   const [file, setFile] = useState();
   const [imageSrc, setImageSrc] = useState();
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [category, setCategory] = useState("pizza");
+  const [prices, setPrices] = useState([]);
+  const [extra, setExtra] = useState("");
+  const [extraOptions, setExtraOptions] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/categories`
+        );
+        setCategories(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, []);
+  // console.log(categories);
 
   const handleOnChange = (changeEvent) => {
     const reader = new FileReader();
@@ -30,9 +55,41 @@ const AddProduct = ({ setIsProductModal }) => {
         "https://api.cloudinary.com/v1_1/coffee-integrify/image/upload",
         data
       );
+      const { url } = uploadRes.data;
+      // console.log(url);
+      const newProduct = {
+        img: url,
+        title,
+        desc,
+        category: category.toLowerCase(),
+        prices,
+        extraOptions,
+      };
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/products`,
+        newProduct
+      );
+      if (res.status === 200) {
+        setIsProductModal(false), toast.success("Product added successfully");
+      }
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleExtra = (e) => {
+    if (extra) {
+      if (extra.text && extra.price) {
+        setExtraOptions([...extraOptions, extra]);
+        setExtra("");
+      }
+    }
+  };
+
+  const changePrice = (e, index) => {
+    const currentPrices = prices;
+    currentPrices[index] = e.target.value;
+    setPrices(currentPrices);
   };
 
   return (
@@ -46,7 +103,7 @@ const AddProduct = ({ setIsProductModal }) => {
               <label className="flex gap-2 items-center">
                 <input
                   type="file"
-                  onChange={(e) => handleOnChange(e)}
+                  onChange={handleOnChange}
                   className="hidden"
                 />
                 <button className="btn-primary !rounded-none !bg-blue-600 pointer-events-none">
@@ -70,13 +127,15 @@ const AddProduct = ({ setIsProductModal }) => {
                 type="text"
                 className="border-2 p-1 text-sm px-1 outline-none"
                 placeholder="Write a title..."
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
             <div className="flex flex-col text-sm mt-4">
-              <span className="font-semibold mb-[2px]">Description</span>
+              <span className="font-semibold mb-[2px]">desc</span>
               <textarea
                 className="border-2 p-1 text-sm px-1 outline-none"
-                placeholder="Write a title..."
+                placeholder="Write a description..."
+                onChange={(e) => setDesc(e.target.value)}
               />
             </div>
 
@@ -85,33 +144,50 @@ const AddProduct = ({ setIsProductModal }) => {
               <select
                 className="border-2 p-1 text-sm px-1 outline-none"
                 placeholder="Write a title..."
+                onChange={(e) => setCategory(e.target.value)}
               >
-                <option value="1">Category 1</option>
-                <option value="1">Category 1</option>
-                <option value="1">Category 1</option>
-                <option value="1">Category 1</option>
+                {categories.length > 0 && categories.map((category) => (
+                  <option key={category._id} value={category.title.toLowerCase()}>
+                    {category.title}
+                  </option>
+                ))}
+                
               </select>
             </div>
 
             <div className="flex flex-col text-sm mt-4 w-full">
               <span className="font-semibold mb-[2px]">Prices</span>
-              <div className="flex justify-between gap-6 w-full md:flex-nowrap flex-wrap">
-                <input
-                  type="number"
-                  className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
-                  placeholder="small"
-                />
-                <input
-                  type="number"
-                  className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
-                  placeholder="medium"
-                />
-                <input
-                  type="number"
-                  className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
-                  placeholder="large"
-                />
-              </div>
+              {category === "pizza" ? (
+                <div className="flex justify-between gap-6 w-full md:flex-nowrap flex-wrap">
+                  <input
+                    type="number"
+                    className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
+                    placeholder="small"
+                    onChange={(e) => changePrice(e, 0)}
+                  />
+                  <input
+                    type="number"
+                    className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
+                    placeholder="medium"
+                    onChange={(e) => changePrice(e, 1)}
+                  />
+                  <input
+                    type="number"
+                    className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
+                    placeholder="large"
+                    onChange={(e) => changePrice(e, 2)}
+                  />
+                </div>
+              ) : (
+                <div className="flex justify-between gap-6 w-full md:flex-nowrap flex-wrap">
+                  <input
+                    type="number"
+                    className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
+                    placeholder="small"
+                    onChange={(e) => changePrice(e, 0)}
+                  />
+                </div>
+              )}
             </div>
             <div className="flex flex-col text-sm mt-4 w-full">
               <span className="font-semibold mb-[2px]">Extra</span>
@@ -120,18 +196,38 @@ const AddProduct = ({ setIsProductModal }) => {
                   type="text"
                   className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
                   placeholder="item"
+                  name="text"
+                  onChange={(e) =>
+                    setExtra({ ...extra, [e.target.name]: e.target.value })
+                  }
                 />
                 <input
                   type="number"
                   className="border-b-2 p-1 pl-0 text-sm px-1 outline-none w-36"
                   placeholder="price"
+                  name="price"
+                  onChange={(e) =>
+                    setExtra({ ...extra, [e.target.name]: e.target.value })
+                  }
                 />
-                <button className="btn-primary ml-auto">Add</button>
+                <button className="btn-primary ml-auto" onClick={handleExtra}>
+                  Add
+                </button>
               </div>
-              <div className="mt-2">
-                <span className="inline-block border border-orange-500 text-orange-500  p-1 rounded-xl text-xs">
-                  Pizza
-                </span>
+              <div className="mt-2 flex  gap-2">
+                {extraOptions.map((item, index) => (
+                  <span
+                    key={index}
+                    onClick={() => {
+                      setExtraOptions(
+                        extraOptions.filter((i) => i.text !== item.text)
+                      );
+                    }}
+                    className="inline-block border cursor-pointer border-orange-500 text-orange-500  p-1 rounded-xl text-xs"
+                  >
+                    {item.text}
+                  </span>
+                ))}
               </div>
             </div>
             <div className="flex justify-end">
