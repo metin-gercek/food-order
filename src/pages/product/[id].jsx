f746cb77cc79bd896a3010016e866ff74e28e18e
metin.gercek <metingercek@hotmail.com>
2023-06-12 17:09:24 +0300

    product detail page

--- src/pages/product/[id].jsx
+++ src/pages/product/[id].jsx
@@ -44,12 +44,12 @@ const Index = ({ food }) => {
   return (
     <div className="flex items-center md:h-[calc(100vh_-_88px)] gap-5 py-20 flex-wrap ">
       <div className="relative md:flex-1 md:w-[80%] md:h-[80%] w-36 h-36 mx-auto">
-        <Image src={food?.img} alt="" layout="fill" objectFit="contain" />
+        <Image src={food?.img} alt="" layout="fill" style={{objectFit:"contain"}} />
       </div>
       <div className="md:flex-1 md:text-start text-center">
         <Title addClass="text-6xl">{food?.title}</Title>
         <span className="text-primary text-2xl font-bold underline underline-offset-1 my-4 inline-block">
-          ${price}
+          € {price}
         </span>
         <p className="text-sm my-4 md:pr-24">{food?.desc}</p>
         <div>
@@ -60,7 +60,7 @@ const Index = ({ food }) => {
                 className="relative w-8 h-8 cursor-pointer"
                 onClick={() => handleSize(0)}
               >
-                <Image src="/images/size.png" alt="" layout="fill" />
+                <Image src="/images/size.png" alt="" fill />
                 <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
                   Small
                 </span>
@@ -69,7 +69,7 @@ const Index = ({ food }) => {
                 className="relative w-12 h-12 cursor-pointer"
                 onClick={() => handleSize(1)}
               >
-                <Image src="/images/size.png" alt="" layout="fill" />
+                <Image src="/images/size.png" alt="" fill />
                 <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
                   Medium
                 </span>
@@ -78,7 +78,7 @@ const Index = ({ food }) => {
                 className="relative w-16 h-16 cursor-pointer"
                 onClick={() => handleSize(2)}
               >
-                <Image src="/images/size.png" alt="" layout="fill" />
+                <Image src="/images/size.png" alt="" fill />
                 <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
                   Large
                 </span>
