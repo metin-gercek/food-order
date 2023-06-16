03f6b7b138c3d5ae20403f1bcdbf74746da78839
metin.gercek <metingercek@hotmail.com>
2023-06-16 16:42:42 +0300

    fetch footer api

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Title from "../ui/Title";
+import axios from "axios";
+import { set } from "mongoose";
 
 const Footer = () => {
+  const [footer, setFooter] = useState([]);
+
+  useEffect(() => {
+    const getFooter = async () => {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/footer`
+      );
+      setFooter(response.data[0]);
+    };
+    getFooter();
+  }, []);
+
+  console.log(footer);
+
   return (
     <div className="bg-secondary text-white">
       <div className="container mx-auto pt-16 pb-6">
@@ -10,71 +26,58 @@ const Footer = () => {
             <Title addClass="text-[30px]">Contact Us</Title>
             <div className="flex flex-col gap-y-2 mt-3">
               <div>
-                <i className="fa fa-map-marker"></i>
-                <span className="inline-block ml-2">Helsinki</span>
+                <a href={footer?.location} target="_blank" >
+                  <i className="fa fa-map-marker"></i>
+                  <span className="inline-block ml-2">Helsinki</span>
+                </a>
               </div>
               <div>
-                <i className="fa fa-phone"></i>
-                <span className="inline-block ml-2">Call +358 461234567</span>
+                <a href={`tel:${footer?.phoneNumber}`}>
+                  <i className="fa fa-phone"></i>
+                  <span className="inline-block ml-2">Call {footer?.phoneNumber}</span>
+                </a>
               </div>
               <div>
-                <i className="fa fa-envelope"></i>
-                <span className="inline-block ml-2">bitelicious@bitelicious.com</span>
+                <a href={`mailto:${footer?.email}`}>
+                  <i className="fa fa-envelope"></i>
+                  <span className="inline-block ml-2">
+                  {footer?.email}
+                  </span>
+                </a>
               </div>
             </div>
           </div>
           <div className="md:flex-1">
             <Title addClass="text-[38px]">BiteLicious</Title>
             <p className="mt-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis harum exercitationem repellat pariatur amet. Exercitationem.
+            {footer?.desc}
             </p>
             <div className="flex items-center justify-center mt-5 gap-x-2">
-              <a
-                href=""
+              {footer?.socialMedia?.map((item) => (
+                <a
+                href={item.link} key={item._id} target="_blank" rel="noreferrer"
                 className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary transition-all"
               >
-                <i className="fa fa-facebook"></i>
+                <i className={item.icon}></i>
               </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary transition-all"
-              >
-                <i className="fa fa-twitter"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary transition-all"
-              >
-                <i className="fa fa-linkedin"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary transition-all"
-              >
-                <i className="fa fa-instagram"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary transition-all"
-              >
-                <i className="fa fa-pinterest"></i>
-              </a>
+              )) }
+       
             </div>
           </div>
           <div className="md:flex-1">
             <Title addClass="text-[30px]">Opening Hours</Title>
             <div className="flex flex-col gap-y-2 mt-3">
               <div>
-                <span className="inline-block ml-2">Everyday</span>
+                <span className="inline-block ml-2">{footer.openingHours?.day}</span>
               </div>
               <div>
-                <span className="inline-block ml-2">10.00 Am -10.00 Pm</span>
+                <span className="inline-block ml-2">{footer.openingHours?.hour}</span>
               </div>
             </div>
           </div>
         </div>
         <p className="text-center mt-10">
-          © 2022 All Rights Reserved By Metin Gercek
+        Copyright &copy; 2023 All Rights Reserved By Metin Gercek
         </p>
       </div>
     </div>
