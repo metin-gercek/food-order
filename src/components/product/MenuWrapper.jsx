ed14ba3f0669f1247b1a1c320e37589f1b831828
metin.gercek <metingercek@hotmail.com>
2023-06-17 10:22:34 +0300

    add view more button to menu

--- src/components/product/MenuWrapper.jsx
+++ src/components/product/MenuWrapper.jsx
@@ -7,6 +7,7 @@ import { set } from "mongoose";
 const MenuWrapper = ({ categoryList, productList }) => {
   const [active, setActive] = useState(0);
   const [filter, setFilter] = useState([]);
+  const [productLimit, setProductLimit] = useState(3);
 
   useEffect(() => {
     setFilter(
@@ -17,20 +18,22 @@ const MenuWrapper = ({ categoryList, productList }) => {
     );
   }, [categoryList, productList, active]);
 
-
   return (
     <div className="container mx-auto  mb-16">
       <div className="flex flex-col items-center w-full">
         <Title addClass="text-[40px]">Our Menu</Title>
         <div className="mt-10">
-        {categoryList &&
+          {categoryList &&
             categoryList.map((category, index) => (
               <button
                 className={`px-6 py-2  rounded-3xl ${
                   index === active && "bg-secondary text-white"
                 }`}
                 key={category._id}
-                onClick={() => setActive(index)}
+                onClick={() => {
+                  setActive(index);
+                  setProductLimit(3);
+                }}
               >
                 {category.title}
               </button>
@@ -38,10 +41,28 @@ const MenuWrapper = ({ categoryList, productList }) => {
         </div>
       </div>
       <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[450px]">
-      {filter.length > 0 &&
-          filter.map((product) => (
-            <MenuItem key={product._id} product={product} />
-          ))}
+        {filter.length > 0 &&
+          filter
+            .slice(0, productLimit)
+            .map((product) => <MenuItem key={product._id} product={product} />)}
+      </div>
+      <div className="flex justify-center w-full mt-8 ">
+        {filter.length <= productLimit ? (
+          <button
+            disabled={filter.length <= productLimit}
+            className="btn-primary"
+            onClick={() => setProductLimit(productLimit + 3)}
+          >
+            No More Product
+          </button>
+        ) : (
+          <button
+            className="btn-primary"
+            onClick={() => setProductLimit(productLimit + 3)}
+          >
+            View More
+          </button>
+        )}
       </div>
     </div>
   );
