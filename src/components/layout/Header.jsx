5d3e63542ba1cc2d78e5090c1cc344167751e273
metin.gercek <metingercek@hotmail.com>
2023-06-18 12:18:52 +0300

    fix hamburger menu error

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -17,23 +17,21 @@ const Header = () => {
 
   return (
     <div
-      className={`h-[5.5rem] z-50 fixed w-full 
-      ${
-        router.asPath === "/" ? "bg-transparent" : "bg-secondary"
+      className={`h-[5.5rem] z-50 relative w-full ${
+        router.asPath === "/" ? "bg-transparent" : "bg-secondary !fixed"
       }`}
     >
       <div className="container mx-auto text-white flex justify-between items-center h-full">
         <Logo />
         <nav
-          className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${
-            isMenuModal === true && "!grid place-content-center"
-          }`}
+          className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden z-50
+          ${isMenuModal === true && "!grid place-content-center"}`}
         >
           <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
             <li
               className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
                 router.asPath === "/" && "text-primary"
-              }`}
+              }`} onClick={() => setIsMenuModal(false)}
             >
               {" "}
               <Link href="/">Home</Link>
@@ -41,7 +39,7 @@ const Header = () => {
             <li
               className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
                 router.asPath === "/menu" && "text-primary"
-              }`}
+              }`} onClick={() => setIsMenuModal(false)}
             >
               {" "}
               <Link href="/menu">Menu</Link>
@@ -49,7 +47,7 @@ const Header = () => {
             <li
               className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
                 router.asPath === "/about" && "text-primary"
-              }`}
+              }`} onClick={() => setIsMenuModal(false)}
             >
               {" "}
               <Link href="/about">About</Link>
@@ -57,7 +55,7 @@ const Header = () => {
             <li
               className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
                 router.asPath === "/reservation" && "text-primary"
-              }`}
+              }`} onClick={() => setIsMenuModal(false)}
             >
               {" "}
               <Link href="reservation">Book Table</Link>
@@ -93,7 +91,8 @@ const Header = () => {
                 }`}
                 size={18}
               />{" "}
-              <span className="w-4 h-4 text-xs grid place-content-center rounded-full bg-primary absolute -top-2 -right-3 text-black font-bold">
+              <span className="px-[5px] text-[10px] rounded-full bg-primary absolute -top-2 -right-3 text-black inline-flex items-center justify-center font-bold">
+                {" "}
                 {cart.products.length === 0 ? "0" : cart.products.length}
               </span>
             </span>{" "}
