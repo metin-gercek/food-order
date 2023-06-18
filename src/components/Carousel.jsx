792c93819c38dce79f6c7b0188f69314ba69ff6c
metin.gercek <metingercek@hotmail.com>
2023-06-18 13:29:52 +0300

    add carousel texts

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -38,36 +38,26 @@ const Carousel = () => {
       <Slider {...settings}>
         <div>
           <div className="mt-48 container mx-auto  text-white flex flex-col items-start gap-y-8">
-            <Title addClass="text-5xl">Fast Food Restaurant</Title>
+            <Title addClass="text-5xl">Indulge in Delicious Delights</Title>
             <p className="text-sm sm:w-2/5 w-full">
-              Indulge in a Flavorful Symphony at BiteLecious! Savor our
-              mouthwatering pizzas, juicy hamburgers, and refreshing beverages
-              for a culinary experience that will leave you craving for more.
+            At Bitelecious, we invite you to experience a world of delectable delights. Our menu is filled with a tantalizing array of fast food and pizza options that will satisfy your cravings. From sizzling burgers to mouthwatering pizzas and everything in between, we have got something for everyone. Step into our restaurant and let your taste buds embark on a flavor-filled adventure.</p>
+            <button className="btn-primary">Order Now</button>
+          </div>
+        </div>
+        <div>
+          <div className="mt-48 container mx-auto  text-white flex flex-col items-start gap-y-8">
+            <Title addClass="text-5xl">Crafted with Passion and Precision</Title>
+            <p className="text-sm sm:w-2/5 w-full">
+            At Bitelecious, we believe that great food starts with great ingredients. That is why we source only the freshest and finest ingredients to create our culinary masterpieces. Our skilled chefs work with passion and precision, infusing each dish with unforgettable flavors. From our perfectly melted cheese to our secret blend of spices, every bite at Bitelecious is a celebration of taste and quality.
             </p>
             <button className="btn-primary">Order Now</button>
           </div>
         </div>
         <div>
           <div className="mt-48 container mx-auto  text-white flex flex-col items-start gap-y-8">
-            <Title addClass="text-5xl">Fast Food Restaurant</Title>
+            <Title addClass="text-5xl">Your Go-To Spot for Quick and Tasty Meals</Title>
             <p className="text-sm sm:w-2/5 w-full">
-              Taste the Perfect Harmony of Pizza, Burgers, and More! BiteLecious
-              offers a delectable assortment of handcrafted pizzas, gourmet
-              burgers, and a delightful range of beverages to satisfy your
-              cravings. Prepare for a tantalizing journey of flavors!
-            </p>
-            <button className="btn-primary">Order Now</button>
-          </div>
-        </div>
-        <div>
-          <div className="mt-48 container mx-auto  text-white flex flex-col items-start gap-y-8">
-            <Title addClass="text-5xl">Satisfy Your Culinary Desires</Title>
-            <p className="text-sm sm:w-2/5 w-full">
-              Delicious Delights Await at BiteLecious! Dive into a world of
-              scrumptious pizzas, juicy hamburgers, and refreshing beverages
-              that will awaken your taste buds. Discover the ultimate blend of
-              flavors and quality ingredients, prepared with love and served
-              with a smile.
+            We understand that in todays fast-paced world, time is of the essence. That is why Bitelecious is committed to providing fast, friendly, and convenient service. Whether you are dining in, taking out, or opting for delivery, we strive to ensure your experience is seamless and enjoyable. Our attentive staff is always ready to assist, making sure you get your hands on our mouthwatering creations without any hassle. Bitelecious is your go-to spot for satisfying your cravings with quick and tasty meals.
             </p>
             <button className="btn-primary">Order Now</button>
           </div>
